4942eea5a61ce3dfb983e15d4b7216212bb52340
sidproj <morisidhraj001@gmail.com>
2022-02-09 20:17:43 +0530

    both payment modals added

---
+++ src/pages/components/paymentInfoModal.jsx
@@ -0,0 +1,77 @@
+import '../../styles/displayInfoModal.css';
+import {AiOutlineCloseCircle} from 'react-icons/all';
+
+const PaymentInfoModal=(props)=>{
+    console.log(props.data);
+    return(<>
+        <div className="dimScreen">
+            <div className="displayInfoContent">
+                <div className="CreateGalleryModalHeader">
+                    <div className="CreateGalleryModalCaption">{props.type}</div>
+                    <AiOutlineCloseCircle onClick={()=>props.closeModal()} size="2.2em" color="#FEB6B6"/>
+                </div>
+                <div className="visitorInfoContainer">
+                    <div className="dataContainer1">
+                        <div className="visitorDataContainer">
+                            <lable className="displayInfoLable">{props.type}ID:</lable>
+                            <input className="displayInfoInput" value={props.data.id} readOnly/>
+                        </div>
+                        <div className="visitorDataContainer">
+                            <lable className="displayInfoLable">TransactionID:</lable>
+                            <input className="displayInfoInput" value={props.data.tID} readOnly/>
+                        </div>
+                    </div>
+
+                    <div className="dataContainer1">
+                        <div className="visitorDataContainer">
+                            <lable className="displayInfoLable">User:</lable>
+                            <input className="displayInfoInput" value={props.data.user} readOnly/>
+                        </div>
+                        <div className="visitorDataContainer">
+                            <lable className="displayInfoLable">Society:</lable>
+                            <input className="displayInfoInput" value={props.data.society} readOnly/>
+                        </div>
+                    </div>
+
+                    <div className="dataContainer1">
+                        <div className="visitorDataContainer">
+                            <lable className="displayInfoLable">Amount:</lable>
+                            <input className="displayInfoInput" value={props.data.amount} readOnly/>
+                        </div>
+                        <div className="visitorDataContainer">
+                            <lable className="displayInfoLable">Late Fee:</lable>
+                            <input className="displayInfoInput" value={props.data.latefee} readOnly/>
+                        </div>
+                    </div>
+
+                    <div className="dataContainer1">
+                        <div className="visitorDataContainer">
+                            <lable className="displayInfoLable">Date of transaction:</lable>
+                            <input className="displayInfoInput" value={props.data.date} readOnly/>
+                        </div>
+                        <div className="visitorDataContainer">
+                            <lable className="displayInfoLable">Due Date </lable>
+                            <input className="displayInfoInput" value={props.data.dueDate} readOnly/>
+                        </div>
+                    </div>
+
+                    {
+
+                        props.type=="Donation"?(<div className="visitorDataContainer">
+                            <lable className="displayInfoLable">Description:</lable>
+                            <textarea className="displayInfoInput2" value={props.data.description} readOnly/>
+                        </div>):(<></>)
+                    }
+                    {
+                        props.action!="show"?(<div className="visitorDataButtons">
+                            <button className="galleryButtonControls">Save</button>
+                            <button className="galleryButtonControls">Cancel</button>
+                        </div>):(<></>)
+                    }
+                </div>
+            </div>
+        </div>
+    </>);
+}
+
+export default PaymentInfoModal;
--- src/pages/components/payments.jsx
+++ src/pages/components/payments.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
 import {AiFillEye,FaTrash,MdModeEditOutline} from 'react-icons/all';
+import PaymentInfoModal from './paymentInfoModal';
 
 const Payments = (props) => {
 
+    const [ displayInfo , setDisplayInfo ] = useState(0);
+    const [ noticeData,setPaymentData] = useState();
+    const [ action,setAction ] = useState();
+    const [ type,setType ]=useState();
+
     return (
         <>
+            { displayInfo ?( <PaymentInfoModal type={type} action={action} data={noticeData} closeModal={setDisplayInfo}/> ):(<></>) }
             <div className="galleryButtons">
             <div className="galleryCaption">Maintenance Transactions</div>
                 {
@@ -25,43 +32,79 @@ const Payments = (props) => {
                 </tr>
                 <Row 
                     sr="1"
+                    id="sdfhajshfkjxhuirah"
                     user="sidhraj Mori"
                     society="soc ABC"
                     tID="xhuirahbkvjkai2349sd"
                     amount="5000"
-                    time="01:10 AM"
+                    time="01:10 AM 01/01/2022"
+                    des="lorem lorem lorem lorem"
                     status="success"
+                    latefee="500"
                     userType={props.userType}
+                    
+                    modal={setDisplayInfo}
+                    changePaymentData={setPaymentData}
+                    changeAction={setAction}
+                    changeType={setType}
+                    type="Maintenance"
                 /> 
                 <Row 
-                    sr="1"
-                    user="sidhraj Mori"
-                    society="soc ABC"
-                    tID="xhuirahbkvjkai2349sd"
-                    amount="5000"
-                    time="01:10 AM"
-                    status="success"
-                    userType={props.userType}
+                     sr="1"
+                     id="sdfhajshfkjxhuirah"
+                     user="sidhraj Mori"
+                     society="soc ABC"
+                     tID="xhuirahbkvjkai2349sd"
+                     amount="5000"
+                     time="01:10 AM 01/01/2022"
+                     des="lorem lorem lorem lorem"
+                     status="success"
+                     latefee="500"
+                     userType={props.userType}
+                    
+                    modal={setDisplayInfo}
+                    changePaymentData={setPaymentData}
+                    changeAction={setAction}
+                    changeType={setType}
+                    type="Maintenance"
                 />
                 <Row 
-                    sr="1"
-                    user="sidhraj Mori"
-                    society="soc ABC"
-                    tID="xhuirahbkvjkai2349sd"
-                    amount="5000"
-                    time="01:10 AM"
-                    status="success"
-                    userType={props.userType}
+                     sr="1"
+                     id="sdfhajshfkjxhuirah"
+                     user="sidhraj Mori"
+                     society="soc ABC"
+                     tID="xhuirahbkvjkai2349sd"
+                     amount="5000"
+                     time="01:10 AM 01/01/2022"
+                     des="lorem lorem lorem lorem"
+                     status="success"
+                     latefee="500"
+                     userType={props.userType}
+                    
+                    modal={setDisplayInfo}
+                    changePaymentData={setPaymentData}
+                    changeAction={setAction}
+                    changeType={setType}
+                    type="Maintenance"
                 />
                 <Row 
-                    sr="1"
-                    user="sidhraj Mori"
-                    society="soc ABC"
-                    tID="xhuirahbkvjkai2349sd"
-                    amount="5000"
-                    time="01:10 AM"
-                    status="success"
-                    userType={props.userType}
+                     sr="1"
+                     id="sdfhajshfkjxhuirah"
+                     user="sidhraj Mori"
+                     society="soc ABC"
+                     tID="xhuirahbkvjkai2349sd"
+                     amount="5000"
+                     time="01:10 AM 01/01/2022"
+                     des="lorem lorem lorem lorem"
+                     status="success"
+                     latefee="500"
+                     userType={props.userType}
+                    
+                    modal={setDisplayInfo}
+                    changePaymentData={setPaymentData}
+                    changeAction={setAction}
+                    changeType={setType}
+                    type="Maintenance"
                 />
             </table>
             <div>
@@ -88,43 +131,79 @@ const Payments = (props) => {
                 </tr>
                 <Row 
                     sr="1"
+                    id="sdfhajshfkjxhuirah"
                     user="sidhraj Mori"
                     society="soc ABC"
                     tID="xhuirahbkvjkai2349sd"
                     amount="5000"
-                    time="01:10 AM"
+                    time="01:10 AM 01/01/2022"
+                    des="lorem lorem lorem lorem"
                     status="success"
+                    latefee="500"
                     userType={props.userType}
+                    
+                    modal={setDisplayInfo}
+                    changePaymentData={setPaymentData}
+                    changeAction={setAction}
+                    changeType={setType}
+                    type="Donation"
+                />
+                <Row 
+                     sr="1"
+                     id="sdfhajshfkjxhuirah"
+                     user="sidhraj Mori"
+                     society="soc ABC"
+                     tID="xhuirahbkvjkai2349sd"
+                     amount="5000"
+                     time="01:10 AM 01/01/2022"
+                     des="lorem lorem lorem lorem"
+                     status="success"
+                     latefee="500"
+                     userType={props.userType}
+                     
+                    modal={setDisplayInfo}
+                    changePaymentData={setPaymentData}
+                    changeAction={setAction}
+                    changeType={setType}
+                    type="Donation"
                 />
                 <Row 
                     sr="1"
+                    id="sdfhajshfkjxhuirah"
                     user="sidhraj Mori"
                     society="soc ABC"
                     tID="xhuirahbkvjkai2349sd"
                     amount="5000"
-                    time="01:10 AM"
+                    time="01:10 AM 01/01/2022"
+                    des="lorem lorem lorem lorem"
                     status="success"
+                    latefee="500"
                     userType={props.userType}
+                    
+                    modal={setDisplayInfo}
+                    changePaymentData={setPaymentData}
+                    changeAction={setAction}
+                    changeType={setType}
+                    type="Donation"
                 />
                 <Row 
                     sr="1"
+                    id="sdfhajshfkjxhuirah"
                     user="sidhraj Mori"
                     society="soc ABC"
                     tID="xhuirahbkvjkai2349sd"
                     amount="5000"
-                    time="01:10 AM"
+                    time="01:10 AM 01/01/2022"
+                    des="lorem lorem lorem lorem"
                     status="success"
+                    latefee="500"
                     userType={props.userType}
-                />
-                <Row 
-                    sr="1"
-                    user="sidhraj Mori"
-                    society="soc ABC"
-                    tID="xhuirahbkvjkai2349sd"
-                    amount="5000"
-                    time="01:10 AM"
-                    status="success"
-                    userType={props.userType}
+                    
+                    modal={setDisplayInfo}
+                    changePaymentData={setPaymentData}
+                    changeAction={setAction}
+                    changeType={setType}
+                    type="Donation"
                 />
             </table>
             <div>
@@ -138,6 +217,17 @@ const Payments = (props) => {
 const Row=(props)=>{
     
     const [hover,setHover] = useState(0);
+    const data={
+        id:props.id,
+        tID:props.tID,
+        user:props.user,
+        amount:props.amount,
+        date:props.time,
+        description:props.des,
+        latefee:props.latefee,
+        society:props.society,
+        dueDate:"02:00 AM 01/01/2022",
+    }
     return(
         <>
             <tr className="notHeading">
@@ -169,6 +259,7 @@ const Row=(props)=>{
                         <AiFillEye size="2em" color={hover=="eye"?"#FEB6B6":"#707A8A"}
                             onMouseOver={()=>setHover("eye")}
                             onMouseLeave={()=>setHover(0)}
+                            onClick={()=>{props.changeAction("show");props.changeType(props.type);props.changePaymentData(data);props.modal(1) }}
                         />
                         {
                             props.userType=="SocietyAdmin"?(<><MdModeEditOutline size="2em" color={hover=="edit"?"#FEB6B6":"#707A8A"}

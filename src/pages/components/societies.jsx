2cc74e4d9461d07d4920eba5c3cd47111f809ff0
kavyeshvohra <vohrakavyesh@gmail.com>
2022-02-09 23:25:18 +0530

    Society Style

--- src/pages/components/societies.jsx
+++ src/pages/components/societies.jsx
@@ -1,14 +1,39 @@
 import {useState} from 'react';
-import '../../styles/fileAndFolder.css';
-import '../../styles/gallery';
-import Folder from './folder';
+import * as SStyle from '../../styles/societies'
+import soc1_img from '../../Images/soc1.jpg'
+import soc2_img from '../../Images/soc2.jpg'
+import soc3_img from '../../Images/soc3.jpg'
+import soc4_img from '../../Images/soc4.jpg'
+// import '../../styles/fileAndFolder.css';
+// import '../../styles/gallery';
+// import Folder from './folder';
 
 
 const Societies = () =>{
-    const [societies,setSocieties] = useState(["soc A","soc B","soc C","soc D","soc E","soc F",]);
+    const societies = ["Siddhachal Flats","Ankur Appartments","Goyal Intercity-C", "Mahasagar Appartments",,"Suryasagar Appartments","Swati Appartments"];
+    const Area = ['Drive In Road', 'Gurukul', 'Mansi Charrasta', 'Bopal', 'Makarba', 'Isckon Cross Roads', 'Kalupur']
+    const Image = [soc1_img,soc2_img,soc3_img,soc4_img,soc1_img,soc3_img]
+    let area = 0;
+    let image = 0 ;
     return(
         <>
-             <div className="galleryHeader">
+            <SStyle.HeadingCont>
+                <SStyle.Heading>Manage Societies</SStyle.Heading>
+            </SStyle.HeadingCont>
+            <SStyle.Society>
+                {societies.map(society => {
+                    return(
+                        <SStyle.SocietyCont>
+                            <SStyle.Image src={Image[image++]}/>
+                        <SStyle.Content>
+                            <h3>{society}</h3>
+                            <p>{Area[area++]}</p>
+                        </SStyle.Content>
+                    </SStyle.SocietyCont>
+                    )
+                })}
+            </SStyle.Society>
+             {/* <div className="galleryHeader">
                 <div className="galleryCaption">Societies</div>
             </div>
             <div className="fileFolderContainer">
@@ -17,7 +42,7 @@ const Societies = () =>{
                         return( <Folder folderName={soc} type="society"  />)
                     })
             }
-            </div>
+            </div> */}
         </>
     );
 }
---
+++ src/styles/societies.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components'
+
+export const Society = styled.div`
+    display: flex;
+    /* justify-content: center; */
+    flex-direction: row;
+    flex-wrap: wrap;
+`
+export const HeadingCont = styled.div`
+    margin: 2em;
+
+`
+export const Row = styled.div`
+`
+export const Heading = styled.h1`
+    text-align: center;
+    text-decoration: underline;
+`
+export const SocietyCont = styled.div`
+    margin: 2em;
+    width: 20em;
+    border: 2px dashed #eee;
+    border-radius: 15px;
+    position: relative;
+    transition: 0.3s all ease-in-out 0s;
+    box-shadow: -1px 1px 11px #00000014;
+    &:hover
+    {
+        border-color: #FAB6B6;
+    }
+`
+export const Content = styled.div`
+    padding: 2em;
+    border-radius: 0 0 15px 15px;
+    transition: 1s all ease-in-out 0;
+    cursor: pointer;
+    & h3
+    {
+        font-size: 1.4em;
+        text-align: center;
+    }
+    & p
+    {
+        text-align: center;
+    }
+    &:hover
+    {
+        background: rgba(255, 232, 232, 0.5);
+        background: -webkit-linear-gradient(top left, rgba(255, 232, 232, 1.0), rgba(140, 147, 159, 1.0));
+        background: -moz-linear-gradient(top left, rgba(255, 232, 232, 1.0), rgba(140, 147, 159, 1.0));
+        background: linear-gradient(to bottom right, rgba(255, 232, 232, 0.5), rgba(140, 147, 159, 0.5));
+    }
+
+`
+export const Image = styled.img`
+width: 100%;
+height: 10em;
+    border-radius: 15px 15px 0px 0px;
+`

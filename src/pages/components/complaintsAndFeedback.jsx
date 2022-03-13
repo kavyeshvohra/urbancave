cffbd78daf01543333c73fda3520a5c924f0fa4a
kavyeshvohra <vohrakavyesh@gmail.com>
2022-03-13 19:31:48 +0530

    Complaints & Feedback Redesigned

--- src/pages/components/complaintsAndFeedback.jsx
+++ src/pages/components/complaintsAndFeedback.jsx
@@ -1,18 +1,60 @@
 import React, {useEffect, useState} from 'react'
 import * as Cstyle from '../../styles/complaints'
 import {BiPencil,BiHelpCircle} from 'react-icons/bi'
+import {AiOutlineSearch} from 'react-icons/ai'
+import {BsThreeDots} from 'react-icons/bs'
+import {Modal, Button, Col, Row} from 'react-bootstrap'
+import {FaUserAlt} from 'react-icons/fa'
+import {MdOutlineCancel} from 'react-icons/md'
+import Select from 'react-select'
+import { Detailsform,FocusHtml, FormGroup, TextSpan, ButtonWrapper,ButtonSubmit, Detailsformdate } from '../../styles/register-styles'
 
+
+const FormStyle = {
+    width: "100%"
+}
+const FormStyle1 = {
+    width: "100%",
+}
+const FormStyle2 = {
+    width: "100%",
+    marginTop: "0.5em",
+}
+const options = [
+    { value: "Society" ,label: "Society "},
+    { value: "General", label: "General Query"},
+    { value: "Payments", label: "Payments"},
+    { value: "Technical Support", label: "Technical Support"},
+    { value: "Violation Issues", label: "Abusive / Improper Content"}
+]
 
 const ComplaintsAndFeedback = () => {
     const [itemColor, setItemColor] = useState("All Tickets")
     const [society, setSocietyName] = useState("Siddhachal Flats")
     const [subColor, setSocietyColor] = useState(0)
+    const [Popup, setPopup] = useState(false);
+
+    const [counter, setCount] = useState(0)
+
     function MenuItem(e){
         setItemColor(e.target.textContent)
     }
     function SocietySelect(e){
         setSocietyName(e.target.textContent)
     }
+
+    const handleKeyPress = (e) => {
+        const count = e.target.value;
+        
+        const countWords = (count) => {
+          if (count.length === 0) {
+            return 0;
+          }
+        }
+        
+        setCount({});
+    }
+
     return (
      <>
      <Cstyle.Wrapper>        
@@ -28,7 +70,8 @@ const ComplaintsAndFeedback = () => {
                 </Cstyle.SocList>
             </Cstyle.SocietyContent>
         </Cstyle.SocietyContainer>
-        <Cstyle.FilterContainer>
+        <Cstyle.ComplaintContainer>
+            <Cstyle.MenuWrapper>
                 <Cstyle.MenuItems>
                     <li onClick={MenuItem}>
                         All Tickets
@@ -43,26 +86,158 @@ const ComplaintsAndFeedback = () => {
                         Feedbacks
                     </li>
                 </Cstyle.MenuItems>
-                <Cstyle.ComplaintHistoryContainer>
-                    <Card/>
-                </Cstyle.ComplaintHistoryContainer>
-        </Cstyle.FilterContainer>
+            </Cstyle.MenuWrapper>
+            <Cstyle.ButtonContainer>
+                <button className='newTicket' onClick={()=>{setPopup(true)}}><BiPencil/> NEW TICKET</button>
+                <div>
+                    <input type="search" placeholder='Search'/>
+                    <button className='search-icon' type="submit"><AiOutlineSearch/></button>
+                </div>
+            </Cstyle.ButtonContainer>
+                <div>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                </div>
+        </Cstyle.ComplaintContainer>
+
     </Cstyle.Wrapper>
+    <Modal show={Popup} onHide={()=>{setPopup(false)}} centered>
+    <Modal.Header closeButton>
+        <Modal.Title>Raise A New Ticket</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+        <div style={{textAlign: "right"}}>
+            <span>User Name: <span style={{fontStyle: "italic", color: "#3e444e", fontWeight: "500"}}>Patel Manikbhai</span></span>
+        </div>
+        <form>
+        <FormGroup style={FormStyle2}>
+            <Detailsform type="text" id="title" name="complaint_title" onChange={inputChange}/>
+            <FocusHtml data-placeholder="Title"/>
+        </FormGroup>
+        <FormGroup style={FormStyle1}>
+            <Select options={options}/>
+            <TextSpan style={{height: "auto"}}>Category</TextSpan>
+        </FormGroup>
+        <FormGroup style={FormStyle}>
+            <Cstyle.ComplaintDesc id="description" name="complaint_desc" maxLength="255" rows="6" onInput={handleKeyPress} />
+            <TextSpan style={{height: "auto"}}>Description</TextSpan>
+            <span style={{float: "right"}}>{counter}/180</span>
+        </FormGroup>
+        <ButtonWrapper style={{marginBottom: "1em"}}>
+            <ButtonSubmit name="submit" type="submit">Submit</ButtonSubmit>
+        </ButtonWrapper>
+        </form>
+    </Modal.Body>
+    </Modal>
     </>
   )
 }
+function inputChange(e) {
+    if (e.target.value !== "") {
+        e.target.classList.add('text');
+    }
+    else {
+        e.target.classList.remove('text');
+    }
+}
 
-const Card = () => {
+const Card = (props) => {
+    const [MoreView, setMoreView] = useState(false);
     return(
         <>
-            <Cstyle.ButtonContainer>
-                <button><BiPencil/> NEW TICKET</button>
-            </Cstyle.ButtonContainer>
+            <Cstyle.ComplaintHistoryContainer>
             <Cstyle.ComplaintInfo>
                 <Cstyle.ComplaintHistoryHeader>
-                    <h3><BiHelpCircle/>Ticket #2022-0111 <span style={{fontWeight: "300", fontSize: "18px"}}>11:30PM</span></h3>
+                    <BiHelpCircle/>
+                    <h5>Ticket #2022-0111</h5>
+                    <span style={{fontWeight: "300", fontSize: "18px"}}>11:30PM</span>
+                    <button onClick={()=>{setMoreView(!MoreView)}}><BsThreeDots/></button>
+                    {MoreView ? <MoreItems/> : null}
                 </Cstyle.ComplaintHistoryHeader>
+                <Cstyle.ComplaintInfoDetails>
+                    <h2>How to change my password?</h2>
+                    <p>I want to change my password? Where is the option?</p>
+                </Cstyle.ComplaintInfoDetails>
+                <Cstyle.ComplaintFooterInfo>
+                    <FaUserAlt/>
+                    <h6 style={{flex: "1"}}>Kavyesh Vohra</h6>
+                    <h6>Category: <span style={{fontStyle: "italic", color: '#707a8a', textDecoration: "underline", textDecorationColor: "#fab0b0", cursor: "pointer"}}>Technical Support</span></h6>
+                </Cstyle.ComplaintFooterInfo>
             </Cstyle.ComplaintInfo>
+            </Cstyle.ComplaintHistoryContainer>
+        </>
+    )
+}
+
+
+const MoreItems = () => {
+    const [DeleteModal, setDeleteModal] = useState(false);
+    const [ViewModal, setViewModal] = useState(false);
+    return(
+        <>
+            <Cstyle.MoreItemsContainer>
+                <ul>
+                    <li onClick={()=>{setViewModal(true)}}>View</li>
+                    <li onClick={()=>{setDeleteModal(true)}}>Delete</li>
+                </ul>
+            </Cstyle.MoreItemsContainer>
+
+            <Modal show={ViewModal} onHide={()=>{setViewModal(false)}} centered size="lg">
+                <Modal.Header closeButton>
+                    <Modal.Title>Complaint Details</Modal.Title>
+                </Modal.Header>
+                <Modal.Body closeButton >
+                    <Cstyle.DetailWrapper>
+                    <Row>
+                    <Col xs={12} md={4}>
+                        <span>ID</span>
+                        <Detailsform type="text" value="cf10001fg" disabled/>
+                    </Col>
+                    <Col xs={12} md={8}>
+                        <span>Title</span>
+                        <Detailsform type="text" value="How to change my password?" disabled/>
+                    </Col>
+                    </Row>
+                    <Row>
+                    <Col xs={12} md={4}>
+                        <span>Category</span>
+                        <Detailsform type="text" value="Technical Support" disabled/>
+                    </Col>
+                    <Col xs={12} md={3}>
+                        <span>Date</span>
+                        <Detailsformdate type="date" disabled value="2022-01-22"/>
+                    </Col>
+                    <Col xs={12} md={5}>
+                        <span>User (<span style={{fontStyle: "italic"}}>Society Member</span>)</span>
+                        <Detailsform type="text" value="Kavyesh Vohra" disabled/>
+                    </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12} md={12}>
+                        <span>Description</span>
+                        <Cstyle.ComplaintDesc disabled rows="6" maxLength="255" value="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,"/>
+                        </Col>
+                    </Row>
+                    </Cstyle.DetailWrapper>
+                </Modal.Body>
+            </Modal>
+
+            <Modal show={DeleteModal} onHide={()=>{setDeleteModal(false)}} centered>
+                <Modal.Header closeButton style={{borderBottom: "none"}}>
+                    {/* <Modal.Title>Delete Complaint</Modal.Title> */}
+                </Modal.Header>
+                <Modal.Body closeButton style={{paddingTop: "unset"}}>
+                    <Cstyle.DeleteCont>
+                        <MdOutlineCancel style={{fontSize: "8em", color: "red"}}/>
+                        <h2>Are you sure?</h2>
+                        <p>Do you really want to delete this record? This process cannot be undone.</p>
+                        <Button variant='outline-danger'>Delete</Button>
+                    </Cstyle.DeleteCont>
+                </Modal.Body>
+            </Modal>
         </>
     )
 }
--- src/styles/complaints.js
+++ src/styles/complaints.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import cancel from '../Images/cancel.svg'    
 // export const CnfHeading = styled.div`
 //     margin: 0.5rem 0rem 1rem 0rem;
 //     fon-ize: 2rem;
@@ -10,10 +11,111 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
     display: flex;
 `
+// export const Span = styled.span`
+//     position: absolute;
+//     pointer-events: none;
+//     transition: 0.3s all ease;
+//     display: block;
+//     width: 100%;
+//     height: 100%;
+//     top: -5px;
+//     left: 35px;
+//     &::after
+//     {
+//         content: attr(data-placeholder);
+//         color: red;
+//         display: block;
+//         width: 100%;
+//         position: absolute;
+//         top: 16px;
+//         left: 0px;
+//         padding-left: 5px;
+
+//         -webkit-transition: all 0.4s;
+//         -o-transition: all 0.4s;
+//         -moz-transition: all 0.4s;
+//         transition: all 0.4s;
+//     }
+// `
+
+export const MoreItemsContainer = styled.div`
+    background-color: #fff;
+    border-radius: 5px;
+    position: absolute;
+    width: 150px;
+    top: 34px;
+    left: 32em;
+    box-shadow: 2px 1px 6px 0px;
+    /* padding: 8px 15px; */
+    & > ul
+    {
+        list-style: none;
+        cursor: pointer;
+        padding: unset;
+        margin-bottom: 0px;
+        & > li:hover
+        {
+            background: #fab6b6;
+        }
+        & > li
+        {
+            padding-left: 20px;
+            padding-top: 10px;
+            padding-right: 20px;
+            padding-bottom: 10px;
+            /* margin-top: 8px; */
+        }
+    }
+`
 export const ButtonContainer = styled.div`
     margin: 2em 4em;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-    & > button
+[type="search"]::-webkit-search-cancel-button {
+  -webkit-appearance: none;
+  appearance: none;
+  height: 10px;
+  width: 10px;
+  background-image: url(${cancel});
+  background-size: 10px 10px;
+  background-repeat: no-repeat;
+}
+    input[type="search"] {
+    border: none;
+    white-space: wrap;
+    border-radius: 5px 0px 0px 5px;
+	background: #E9E9E9;
+	overflow: hidden;
+	text-overflow: clip;
+	padding: 0.35em 0.75em;
+	border: none;
+	font-size: 1.1em;
+	text-decoration: none;
+	line-height: normal;
+	max-height: 3em;
+  
+    flex: 2 0 250px;
+    }
+
+    input[type="search"]:focus {
+        background: rgba(250, 182, 182, 0.1);
+        color: black;
+        transition: background 400ms ease-in-out;
+        outline: none;
+}
+    .search-icon
+    {
+        background: none;
+        padding: 5px 10px;
+        transition: 0.2s ease-in all;
+        background-color: #3e444e;
+        color: #fab6b6;
+        font-weight: 600;
+        border: none;
+    }
+    .newTicket
     {
         display: flex;
         align-items: center;
@@ -46,9 +148,27 @@ export const SocietyContainer = styled.div`
     background-color: #eee;
     height: 100vh;
     padding-top: 2.5em;
+    position: fixed;
 `
 export const SocietyContent = styled.div`
     padding-bottom: 20px;
+`
+export const DeleteCont = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    & > h2
+    {
+        margin-bottom: 1em;
+        margin-top: .5em;
+    }
+    & > p
+    {
+        width: 80%;
+        text-align: center;      
+    }
 `
 export const SocList = styled.ul`
     list-style: none;
@@ -77,15 +197,35 @@ export const SocList = styled.ul`
     }
 
 `
+export const InputViewCont = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 1em;
+    width: 30%;
+`
+export const DetailWrapper =styled.div`
+    display: flex;
+    gap: 1em;
+    flex-direction: column;
+`
 export const SocietyHead = styled.div`
-    margin-bottom: 2.5em;
+    margin-bottom: 1.5em;
     text-align: center;
     color: #000;
     letter-spacing: 1px;
+    padding-bottom: 20px;
+    border-bottom: 2px solid #fab0b0;
 `
-export const FilterContainer = styled.div`
+export const ComplaintContainer = styled.div`
     flex: 2;
     margin-top: 2.5em;
+    position: relative;
+    margin-left: 24em;
+`
+export const MenuWrapper = styled.div`
+    margin-left: 20px;
+    border-bottom: 2px dashed #fab0b0;
 `
 export const MenuItems = styled.ul`
     list-style: none;
@@ -108,27 +248,30 @@ export const MenuItems = styled.ul`
         color:#fab0b0;
     }
 `
-export const ComplaintSubject = styled.select`
-    width: 25.3rem;
-    height: fit-content;
-    outline: none;
-    color:#3e444e;
-    border: #FAB6B6 solid 0.14rem;
-    background-color: #FFFEFE;
-    font-size: 1rem;
-    padding: 0.25rem;
-    border-radius: 0.5rem;
+export const ComplaintDesc = styled.textarea`
+    cursor: text;
+    width: 100%;
+    padding: 8px;
+    resize: none;
+    border-radius: 5px;
+    border: 2px solid #000;
 `
 
 export const ComplaintInfo = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;
-    width: inherit;
+    justify-content:space-between;
+    max-height: 15rem;
+    /* align-items: center; */
+    /* width: inherit; */
     /* border: #FAB6B6 solid 0.14rem; */
     background-color: rgba(112,122,138,0.2);
     height: 15rem;
     border-radius: 0.5rem;
+    padding-top: 8px;
+    padding-left: 30px;
+    padding-right:20px;
+    padding-bottom: 20px;
 `
 
 export const ComplaintInfoHeading = styled.div`
@@ -143,22 +286,15 @@ export const ComplaintInfoHeading = styled.div`
     pointer-events: none;
 `
 
-export const ComplaintInfoDetails = styled.textarea`
-    font-size: 1rem;
-    padding: 0.25rem;
-    width: 24.40rem;
-    text-align: left;
-    outline: none;
-    border: none;
-    background-color: #FFFEFE;
-    resize: none;
-    color:#3e444e;
-    height: 13rem;
-
-    &::placeholder{
-        color:#676767;
-        font-size: 1rem;
-        
+export const ComplaintInfoDetails = styled.div`
+    & > h2
+    {
+        text-align: left;
+    }
+    & > p
+    {
+        white-space: normal;
+        text-overflow: ellipsis;
     }
 `
 
@@ -199,37 +335,55 @@ export const ComplaintHistoryHeaderContainer = styled.div`
 `
 
 export const ComplaintHistoryHeader = styled.div`
-    font-size: 2rem;
     margin: 0.5em 0em;
+    position: relative;
     display: flex;
     justify-content: space-around;
-    align-items:center;
+    align-items: center;
     text-align: left;
-    margin-left: 1.5rem;
+    /* margin-left: 1.5rem; */
     font-weight: 400;
     color:#3e444e;
-    width: 50%;
-`
-
-export const ComplaintHistoryCategory = styled.select`
-    width: rem;
-    height: fit-content;
-    outline: none;
-    color:#3e444e;
-    border: #FAB6B6 solid 0.14rem;
-    background-color: #FFFEFE;
-    font-size: 1rem;
-    padding: 0.25rem;
-    border-radius: 0.5rem;
-`
-
-export const ComplaintTicketContainer = styled.div`
-    margin-top: 4rem;
     width: 100%;
-    display: grid;
-    grid-template-columns: auto auto auto auto auto auto auto;
-    row-gap: 1em;
-    justify-content: space-evenly;
+    max-width: 100%;
+    padding-top: .25em;
+    gap: 1em;
+    & > svg
+    {
+        font-size: 18px;
+    }
+    & > h5
+    {
+        flex: 1;
+        margin: 0;
+        font-style: italic;
+        color: #F89E9E;
+    }
+    & > button
+    {
+        padding-bottom: 2px;
+        border: none;
+        border-radius: 5px;
+        width: 35px;
+        background: none;
+        border: 2px solid #fab0b0;
+        transition: 0.2s ease-in;
+        &:hover
+        {
+            background-color: #fab0b0;
+        }
+    }
+`
+
+export const ComplaintFooterInfo = styled.div`
+    display: flex;
+    align-items:center;
+    justify-content: space-between;
+    gap: 1em;
+    & > h6
+    {
+        margin: 0;
+    }
 `
 export const ComplaintTicketControls = styled.div`
     display: flex;
